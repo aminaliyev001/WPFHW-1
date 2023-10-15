51696b0b4f2df0bfcdca2860e7b003f09c0f62ed
aminaliyev001 <72746607+aminaliyev001@users.noreply.github.com>
2023-10-15 13:18:12 +0400

    Add files via upload

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Random random = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn != null)
+            {
+                btn.Background = new SolidColorBrush(Color.FromRgb((byte)random.Next(256), (byte)random.Next(256), (byte)random.Next(256)));
+                MessageBox.Show($"content: {btn.Content}, name:{btn.Name}, font size: {btn.FontSize}, Margin: {btn.Margin}" +
+                    $"Padding: {btn.Padding}" +
+                    $"Width: {btn.Width}");
+            }
+        }
+
+        private void Button_RightClick(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn != null)
+            {
+                this.Title = $"{btn.Content}";
+                btn.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
